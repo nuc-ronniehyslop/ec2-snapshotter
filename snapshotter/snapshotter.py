e343794794929ddf84f82abd6a444a796b632888
Ronnie Hyslop <ronnie.hyslop@nucleusfinancial.com>
2020-05-29 15:26:09 +0000

    Enhanced list

--- snapshotter/snapshotter.py
+++ snapshotter/snapshotter.py
@@ -1,8 +1,29 @@
 import boto3
+import click
+
+session = boto3.Session()
+ec2 = session.resource('ec2')
+
+
+@click.command()
+def list_instances():
+    "List EC2 Instances"
+    for i in ec2.instances.all():
+        name = ''
+        for tag in i.tags:
+            if tag['Key'] == 'Name':
+                name = tag['Value']
+      
+            
+        print(', '.join((
+            i.id,
+            name, 
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
+
 
 if __name__ == '__main__':
-    session = boto3.Session()
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
